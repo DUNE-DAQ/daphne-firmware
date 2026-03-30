2dfe25981e1390808b0e49c904a1e3d7e4524f6d
marroyav <matheos.mc2@gmail.com>
2026-03-30 12:38:20 +0200

    Add Vivado preflight and fix WSL launcher env propagation

--- scripts/fusesoc/run_vivado_batch.sh
+++ scripts/fusesoc/run_vivado_batch.sh
@@ -32,4 +32,33 @@ export DAPHNE_BOARD_PART
 export DAPHNE_PFM_NAME
 
 cd "$ROOT_DIR/xilinx"
-exec vivado -mode batch -source vivado_batch.tcl
+
+shim_tcl=".daphne-vivado-shim.$$.tcl"
+trap 'rm -f "$shim_tcl"' EXIT INT TERM HUP
+
+append_env_tcl() {
+  var_name="$1"
+  eval "var_value=\${$var_name-}"
+  [ -n "$var_value" ] || return 0
+  escaped_value=$(printf '%s' "$var_value" | sed 's/\\/\\\\/g; s/"/\\"/g')
+  printf 'set ::env(%s) "%s"\n' "$var_name" "$escaped_value" >>"$shim_tcl"
+}
+
+: >"$shim_tcl"
+append_env_tcl DAPHNE_FPGA_PART
+append_env_tcl DAPHNE_BOARD_PART
+append_env_tcl DAPHNE_PFM_NAME
+append_env_tcl DAPHNE_BOARD
+append_env_tcl DAPHNE_ETH_MODE
+append_env_tcl DAPHNE_MAX_THREADS
+append_env_tcl DAPHNE_SKIP_POST_SYNTH_REPORTS
+append_env_tcl DAPHNE_SKIP_POST_SYNTH_CHECKPOINT
+append_env_tcl DAPHNE_SYNTH_DIRECTIVE
+append_env_tcl DAPHNE_OPT_DIRECTIVE
+append_env_tcl DAPHNE_PLACE_DIRECTIVE
+append_env_tcl DAPHNE_POST_PLACE_PHYSOPT_DIRECTIVE
+append_env_tcl DAPHNE_ROUTE_DIRECTIVE
+append_env_tcl DAPHNE_POST_ROUTE_PHYSOPT_DIRECTIVE
+printf 'source -notrace [file join [pwd] "vivado_batch.tcl"]\n' >>"$shim_tcl"
+
+exec vivado -mode batch -source "$shim_tcl"
